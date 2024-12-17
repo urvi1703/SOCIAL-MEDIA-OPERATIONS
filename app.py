d5e9de98e1401e78c7734a168371030fb301e5bc
Urvashi Gangal <117017875+urvi1703@users.noreply.github.com>
2024-12-17 15:08:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from create_post import create_reddit_post
 
 #reddit credentials 
 reddit = praw.Reddit(
-    client_id="K1rhO5hjcgWr86L0kW5pjQ",
-    client_secret="KVWREayY7U1_OFzkQlKz00AFW0rXQQ",
-    user_agent="script:crud:v1.0 (by u/Holiday-Box3743)",
-    username="Holiday-Box3743",
-    password="soumya2854"
+    client_id="bqwbGIUUup1mn3_Pt_cyiw",
+    client_secret="GvMAb-9Y3arpcM34VsoybWJ6VqrTgQ",
+    user_agent="script:crud:v1.0 (by /u/BigRaspberry2255)",
+    username="BigRaspberry2255",
+    password="reddit17"
 )
 
 # Streamlit App
